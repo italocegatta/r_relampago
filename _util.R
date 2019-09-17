ceb22f06670599e7c20c13cf7bdfba973b67929c
italocegatta <italocegatta@gmail.com>
2019-09-16 21:52:50 -0300

    Initial commit

---
+++ _util.R
@@ -0,0 +1,16 @@
+set.seed(3005)
+options(digits = 7, scipen = 10000)
+
+knitr::opts_chunk$set(
+ comment = "#>",
+ collapse = TRUE,
+ cache = TRUE,
+ eval = FALSE,
+ message = FALSE, 
+ warning = FALSE
+ # out.width = "70%",
+ # fig.align = 'center',
+ # fig.width = 6,
+ # fig.asp = 0.618,  # 1 / phi
+ # fig.show = "hold"
+)
